60f019c47baf853eb482c56fabdd4bafbd5adcc2
Ubuntu <ubuntu@ip-172-31-33-18.ec2.internal>
2016-06-17 00:44:39 +0000

    fix inti

--- init.sh
+++ init.sh
@@ -2,4 +2,4 @@
 
 name=$(curl http://169.254.169.254/latest/user-data)
 
-sed -i "s/boonhq.com\//boonhq.com\/$name/g" /home/ubuntu/boon-ghost-admin/config.js
+sed -i "s/boonhq.com\//boonhq.com\/$name/g" /home/ubuntu/Ghost/config.js
